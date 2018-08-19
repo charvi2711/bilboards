940c61c98361739064a54fd8d034662660cc64a1
Rahul Kashyap <rahulkashyap1506@gmail.com>
2018-08-20 01:44:10 +0530

    update

--- REST api/file.py
+++ REST api/file.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, Markup
 from werkzeug import secure_filename
 app = Flask(__name__)
 
@@ -11,8 +11,14 @@ def upload_file():
 def upload_file2():
    if request.method == 'POST':
       f = request.files['file']
-      f.save(secure_filename('test'+f.filename))
-      return 'file uploaded successfully'
-		
+      long = request.form['longitude']
+      lat = request.form['latitude']
+      ext = f.filename.split(sep='.')
+      f.save(secure_filename('img.'+ext[-1]))
+      #message = Markup('<a href="http://192.168.43.19/hackinfi/index.php/user_authentication>Click Here</a>')
+      #flash(message)
+      #return render_template('http://192.168.43.19/hackinfi/index.php/user_authentication')
+      return 'file uploaded successfully ' + long + "" + lat
+      		
 if __name__ == '__main__':
    app.run(host='0.0.0.0', debug = False)
